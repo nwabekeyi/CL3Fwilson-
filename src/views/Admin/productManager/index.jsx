3cf525495f644db0b3f5efff6ffb1edb89ee2e30
Nwabekeyi <chidi90simeon@gmail.com>
2025-06-04 20:14:14 +0100

    polling from lacciman

--- src/views/Admin/productManager/index.jsx
+++ src/views/Admin/productManager/index.jsx
@@ -189,7 +189,7 @@ const ProductManager = () => {
               </div>
               <div className="product-details">
                 <h3>{product.name}</h3>
-                <p>Price: ${product.price}</p>
+                <p>Price: ₦{product.price}</p>
                 <p>Department: {product.department}</p>
                 <button
                   onClick={() => handleDelete(product.id)}
